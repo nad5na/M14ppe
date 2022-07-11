fa3bb8f9d2d5e6180de497ecf706f9059707ecaf
Neil Antrassian <neilantrassian@Neils-MacBook-Pro.local>
2022-07-11 10:47:35 -0400

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/neilantrassian/M14ppe',
+      author='Noah Dunn, Neil Antrassian',
+      author_email='mst8gd@virginia.edu',
+      license='MIT',
+      packages=['demo'])
